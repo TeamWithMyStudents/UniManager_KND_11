12244126b13e140786cd0056e0b94b0d8333c181
defbullfinch <bullfincher2008@gmail.com>
2026-02-25 22:30:06 +0200

    Created class Student extends User. Added field private String group (in class Student). Implemented constructor and initialized group (in class Student). Implemented getter for group (in class Student). Overrided toString() for group field (in class Student).
    Created class StudentServiceImpl extends UserServiceImpl.
    Initialized the specific array in the constructor (in StudentServiceImpl).
    Added method findByGroup (in StudentService)
    Implement method findByGroup (in StudentServiceImpl).
    
    2.1
    Added in StudentService extends UserService.
    created constructor super(new Student[5]) to initialize the storage (in StudentServiceImpl).
    Implemented findByGroup logic (iterate through repository, cast to Student, check group).

--- src/main/java/ua/knd11/model/User.java
+++ src/main/java/ua/knd11/model/User.java
@@ -34,6 +34,6 @@ public abstract class User {
 
     @Override
     public String toString() {
-        return "Id: " + getId() + ", Name: " + getName() + ", Surname: " + getSurname() + " \n";
+        return "Id: " + getId() + ", Name: " + getName() + ", Surname: " + getSurname() ;
     }
 }
