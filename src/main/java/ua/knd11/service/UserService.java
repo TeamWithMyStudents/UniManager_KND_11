275af609662d0561a3753b81fea5e444c5bcb072
Raysout <vladkots46@gmail.com>
2026-02-27 08:49:31 +0200

    Reconstruct method getAll

--- src/main/java/ua/knd11/service/UserService.java
+++ src/main/java/ua/knd11/service/UserService.java
@@ -4,5 +4,9 @@ import ua.knd11.model.User;
 
 public interface UserService {
     void add(User u);
+    void delete(int id);
     void getAll();
+    void findByName(String query);
+    void findBySurname(String query);
+    void sortBySurname();
 }
