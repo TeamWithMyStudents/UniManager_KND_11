c034ab48383cb691ca382705fe8abc8865764f40
defbullfinch <bullfincher2008@gmail.com>
2026-02-27 14:31:57 +0200

    deleted imports

--- src/main/java/ua/knd11/UniManager.java
+++ src/main/java/ua/knd11/UniManager.java
@@ -1,10 +1,5 @@
 package ua.knd11;
 
-import ua.knd11.model.Student;
-import ua.knd11.model.User;
-import ua.knd11.service.StudentService;
-import ua.knd11.service.impl.StudentServiceImpl;
-
 public class UniManager {
     public static void main(String[] args) {
     }
