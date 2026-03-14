b222a8ead4522836dc4e1c30dd2b20892369442c
Raysout <vladkots46@gmail.com>
2026-03-14 13:48:37 +0200

    Added Teacher Skeleton Added logic for methods calculateTotalSalary, filterByDegree Override method add from UserServiceImpl

---
+++ src/main/java/ua/knd11/model/Teacher.java
@@ -0,0 +1,43 @@
+package ua.knd11.model;
+
+public class Teacher extends User{
+    private String department;
+    private String degree;
+    private double salary;
+
+    public Teacher(String name, String surname, String department, String degree, double salary) {
+        super(name, surname);
+        this.department = department;
+        this.degree = degree;
+        this.salary = salary;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getDegree() {
+        return degree;
+    }
+
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString(){
+        return "|TId: " + getId() + "|-|TName: " + getName() + "|-|TSurname: " + getSurname() + "|-|TDepartment: " + getDepartment() + "|-|TDegree: " + getDegree() + "|-|TSalary: " + getSalary() + "|\n";
+    }
+}
---
+++ src/main/java/ua/knd11/service/impl/TeacherServiceImpl.java
@@ -0,0 +1,39 @@
+package ua.knd11.service.impl;
+
+import ua.knd11.model.Teacher;
+import ua.knd11.model.User;
+import ua.knd11.service.TeacherService;
+
+public class TeacherServiceImpl extends UserServiceImpl implements TeacherService {
+
+    public TeacherServiceImpl(Teacher[] initialArray) {
+        super(initialArray);
+    }
+
+    public void calculateTotalSalary(){
+        double result = 0;
+        for(User user : repository){
+            Teacher t = (Teacher) user;
+            result += t.getSalary();
+        }
+        System.out.println("Total University Budget: " + result);
+    }
+
+    public void filterByDegree(String degree){
+        for (User user : repository){
+            Teacher t = (Teacher) user;
+            if ( t != null && (t.getDegree().equalsIgnoreCase(degree) || t.getDegree().toLowerCase().contains(degree.toLowerCase()))){
+                System.out.print(t);
+            }
+        }
+    }
+
+    @Override
+    public void add(User u) {
+        Teacher t = (Teacher) u;
+        if (t.getSalary() < 0) {
+            System.out.println("Error salary in: " + t);
+        } else
+            super.add(t);
+    }
+}
