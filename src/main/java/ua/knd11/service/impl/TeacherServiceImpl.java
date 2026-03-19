cd38f61406f275c2671d3871cec337d79c41157d
Lucyechkaaa <64725689+Lucyechkaaa-dev@users.noreply.github.com>
2026-03-19 22:24:02 +0200

    #5,6 teacher domain skeleton and logic (#27)
    * Added Teacher Skeleton
    Added logic for methods calculateTotalSalary, filterByDegree
    Override method add from UserServiceImpl
    
    * Added null checks and improved output view for TeacherServiceImpl.java
    
    * Refactored `TeacherServiceImpl`: added more null checks, improved validation, and enhanced output
    
    * Improved field validation in `TeacherServiceImpl` by adding null checks and enhancing empty string handling
    
    * Enhanced validation in `TeacherServiceImpl`: replaced `System.out` logs with `IllegalArgumentException`, added `isBlank` checks for `filterByDegree` method.
    
    * Refined salary validation message in `TeacherServiceImpl` to clarify requirement for positive value
    
    * Switched from `isEmpty` to `isBlank` for improved field validation in `TeacherServiceImpl`
    
    ---------
    
    Co-authored-by: Raysout <vladkots46@gmail.com>

---
+++ src/main/java/ua/knd11/service/impl/TeacherServiceImpl.java
@@ -0,0 +1,47 @@
+package ua.knd11.service.impl;
+
+import ua.knd11.model.Teacher;
+import ua.knd11.model.User;
+import ua.knd11.service.TeacherService;
+
+public class TeacherServiceImpl extends UserServiceImpl implements TeacherService {
+
+    public TeacherServiceImpl() {
+        super(new Teacher[5]);
+    }
+
+    public void calculateTotalSalary() {
+        double result = 0;
+        for (User user : repository) {
+            if (!(user instanceof Teacher t)) continue;
+            result += t.getSalary();
+        }
+        System.out.println("Total University Budget: " + result);
+    }
+
+    public void filterByDegree(String degree) {
+        if (degree == null || degree.isBlank()) return;
+        for (User user : repository) {
+            if (!(user instanceof Teacher t)) continue;
+            if (t.getDegree().equalsIgnoreCase(degree) || t.getDegree().toLowerCase().contains(degree.toLowerCase())) {
+                System.out.println("\n" + t);
+            }
+        }
+    }
+
+    @Override
+    public void add(User u) {
+        if (u == null) {
+            throw new IllegalArgumentException("User cannot be null");
+        } else if (!(u instanceof Teacher t)) {
+            throw new IllegalArgumentException("Expected Teacher instance");
+        } else if (t.getName() == null || t.getName().isBlank() || t.getSurname() == null || t.getSurname().isBlank() || t.getDepartment() == null || t.getDepartment().isBlank() || t.getDegree() == null || t.getDegree().isBlank()) {
+            throw new IllegalArgumentException("Teacher has empty required fields: " + t);
+        } else if (t.getSalary() <= 0) {
+            throw new IllegalArgumentException("Teacher salary must be positive: " + t);
+        } else {
+            System.out.println("Teacher added: " + t);
+            super.add(t);
+        }
+    }
+}
