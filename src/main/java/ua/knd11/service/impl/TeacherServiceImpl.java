81d675bc3a8824aafe47c66b1b90e5a641211c0c
lucya <lucyafersh@gmail.com>
2026-03-19 20:23:02 +0200

    Refactored `TeacherServiceImpl`: added more null checks, improved validation, and enhanced output

--- src/main/java/ua/knd11/service/impl/TeacherServiceImpl.java
+++ src/main/java/ua/knd11/service/impl/TeacherServiceImpl.java
@@ -4,38 +4,55 @@ import ua.knd11.model.Teacher;
 import ua.knd11.model.User;
 import ua.knd11.service.TeacherService;
 
+import java.util.Objects;
+
 public class TeacherServiceImpl extends UserServiceImpl implements TeacherService {
 
-    public TeacherServiceImpl(Teacher[] initialArray) {
-        super(initialArray);
+    public TeacherServiceImpl() {
+        super(new Teacher[5]);
     }
 
-    public void calculateTotalSalary(){
+    public void calculateTotalSalary() {
         double result = 0;
-        for(User user : repository){
-            if (user == null) continue;
-            Teacher t = (Teacher) user;
+        for (User user : repository) {
+            if (!(user instanceof Teacher t)) continue;
             result += t.getSalary();
         }
         System.out.println("Total University Budget: " + result);
     }
 
-    public void filterByDegree(String degree){
-        for (User user : repository){
-            if (user == null) continue;
-            Teacher t = (Teacher) user;
-            if (t.getDegree().equalsIgnoreCase(degree) || t.getDegree().toLowerCase().contains(degree.toLowerCase())){
-                System.out.println(t);
+    public void filterByDegree(String degree) {
+        if (degree == null) return;
+        for (User user : repository) {
+            if (!(user instanceof Teacher t)) continue;
+            if (t.getDegree().equalsIgnoreCase(degree) || t.getDegree().toLowerCase().contains(degree.toLowerCase())) {
+                System.out.println("\n" + t);
             }
         }
     }
 
     @Override
     public void add(User u) {
-        Teacher t = (Teacher) u;
-        if (t.getSalary() < 0) {
-            System.out.println("Error salary in: " + t);
-        } else
-            super.add(t);
+        if (u == null) {
+            System.out.println("User cannot be null");
+            return;
+        }
+        if (!(u instanceof Teacher t)) {
+            System.out.println("Expected Teacher instance");
+            return;
+        }
+        if (Objects.equals(t.getName(), "") ||
+                Objects.equals(t.getSurname(), "") ||
+                Objects.equals(t.getDepartment(), "") ||
+                Objects.equals(t.getDegree(), "")
+        ) {
+            System.out.println("\n" + t + " isn't added" + "\nSome fields are empty");
+            return;
+        } else if (t.getSalary() <= 0) {
+            System.out.println("\n" + t + " isn't added" + "\nSalary must be positive");
+            return;
+        }
+        System.out.println("Teacher added: " + t);
+        super.add(t);
     }
 }
