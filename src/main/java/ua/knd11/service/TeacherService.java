c1807366c209bacf9b6cf0e948919a8b2a552c60
lucya <lucyafersh@gmail.com>
2026-06-07 00:38:08 +0300

    Quick fix Removed unused methods from TeacherService` interface

--- src/main/java/ua/knd11/service/TeacherService.java
+++ src/main/java/ua/knd11/service/TeacherService.java
@@ -2,18 +2,10 @@ package ua.knd11.service;
 
 import ua.knd11.model.Teacher;
 
-import java.util.ArrayList;
-
 public interface TeacherService {
     void calculateTotalSalary();
 
     Teacher createTeacherWithParts(String[] parts) throws IllegalArgumentException;
 
     void filterByDegree(String degree);
-
-    void addTeacher(Teacher teacher);
-
-    void deleteTeacher(int id);
-
-    ArrayList<Teacher> getAllTeachers();
 }
